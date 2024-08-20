52deebf67e21e4742a30c3e9ad0b6a32dc0e8256
janisgeegee@gmail.com <janisgeegee@gmail.com>
2024-08-20 20:28:10 +0800

    Added function for hovering side number bar

--- view/cell_control_buttons.py
+++ view/cell_control_buttons.py
@@ -7,11 +7,12 @@ class Cell_control_buttons:
     self._screen = screen
     self._button_size= button_size
     self._cell_control_num = num
-    self._cell_control_button = None
     self._cell_control_button_fill = (250, 188, 157)
     self._cell_control_button_text_color = (0, 0, 0)
     self._cell_control_button_left = None
     self._cell_control_button_top = None
+    self._cell_control_button = None
+    self._hover = False
 
 
   def create_cell_control_button(self, top, left):
@@ -20,20 +21,29 @@ class Cell_control_buttons:
     self._cell_control_button_left = left
     self._cell_control_button_top = top
 
+    if self._hover: # if button hover True
+      self._cell_control_button_fill = (185, 250, 191) # Light Green
+    else:
+      self._cell_control_button_fill = (250, 188, 157) # Light orange
+
     # Draw one button
     pygame.draw.rect(screen, self._cell_control_button_fill,[left, top, button_width, button_width])
 
     # Create button react position
-    self._cell_button =  pygame.Rect((left, top),(button_width, button_width))
+    self._cell_control_button =  pygame.Rect((left, top),(button_width, button_width))
 
     # Create and display represent number on buttons
     text = f"{self._cell_control_num}"
     display_text_center_at_buttons(self._screen, text, self._cell_control_button_text_color, button_width, top, left)
 
   def cell_control_button_event(self, event):
-    left = self._cell_control_button_left = left
-    top = self._cell_control_button_top
+    if event.type == pygame.MOUSEMOTION and self._cell_control_button:
+
+      pos = pygame.mouse.get_pos()
+      hit = self._cell_control_button.collidepoint(pos)
+      if hit:
+        self._hover = True
+      else:
+        self._hover = False
+      
     
-    pos = pygame.mouse.get_pos()
-    # hit = self.rect.collidepoint(pos)
-    print(pos)
